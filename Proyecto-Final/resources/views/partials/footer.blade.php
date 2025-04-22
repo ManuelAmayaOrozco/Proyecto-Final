121c73c5f3ad28240878734aa8a0b9950882cad9
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-04-22 13:24:19 +0200

    Footer decorado

--- Proyecto-Final/resources/views/partials/footer.blade.php
+++ Proyecto-Final/resources/views/partials/footer.blade.php
@@ -1,8 +1,18 @@
 <footer class="body__footer">
-    <div class="footer__div_logo">
-        logo
+    <div class="footer__div_contactos">
+        <h5>Contactos</h5>
+        <p>+34-874-234-234</p>
+    </div>
+    <div class="footer__div_redes">
+        <h5>Redes sociales</h5>
+        <ul>
+            <li><a href="">Twitter</a></li>
+            <li><a href="">Youtube</a></li>
+            <li><a href="">Facebook</a></li>
+        </ul>
     </div>
     <div class="footer__div_info">
-        info
+        <h5>Info adicional</h5>
+        <address>C/Mercedes nº 4 Zaragoza</address>
     </div>
 </footer>
