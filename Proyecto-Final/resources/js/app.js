f49a4461d7b2618681cf633f40fbcc340b666abc
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-06-02 19:19:42 +0200

    arreglo resources/js/app.js

--- Proyecto-Final/resources/js/app.js
+++ Proyecto-Final/resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import '../css/partials_styles/reset_styles.css';
+import '../css/partials_styles/font_styles.css';
+import '../css/partials_styles/footer_styles.css';
+import '../css/partials_styles/header_styles.css';
