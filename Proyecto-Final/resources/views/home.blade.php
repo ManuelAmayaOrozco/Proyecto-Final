40440e1e13acfb9522250fcec80362a8c809ccc6
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-06-02 21:20:17 +0200

    new welcome fixed for build

--- Proyecto-Final/resources/views/home.blade.php
+++ Proyecto-Final/resources/views/home.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/alpine.js', 'resources/js/app.js'])
     @include('partials.include-styles')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <title>Bienvenido a BugBuds</title>
