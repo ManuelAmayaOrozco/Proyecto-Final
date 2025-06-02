285017fb99e06e293cfd3ce55b7f6e146e84cad1
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-06-02 19:42:49 +0200

    app.js actualizado 2

--- Proyecto-Final/resources/views/partials/include-styles.blade.php
+++ Proyecto-Final/resources/views/partials/include-styles.blade.php
@@ -1,4 +1,4 @@
-<!-- GEOLOGICA FONT -->
+<!-- GOOGLE FONTS -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Geologica:wght@100..900&family=Gothic+A1&display=swap" rel="stylesheet">
@@ -9,12 +9,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
+
 <!-- FLATPICKR -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
-<!-- MY STYLES -->
-@vite('resources/css/partials_styles/font_styles.css')
-@vite('resources/css/partials_styles/footer_styles.css')
-@vite('resources/css/partials_styles/header_styles.css')
-@vite('resources/js/datepickerinitialzr.js')
+<!-- VITE -->
+@vite('resources/js/app.js')
