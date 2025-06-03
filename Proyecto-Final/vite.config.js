d2118c4179aa482798a79d45a43555e77ca45ab5
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-06-03 13:34:16 +0200

    El resto del css

--- Proyecto-Final/vite.config.js
+++ Proyecto-Final/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/editor.js',
                 'resources/js/alpine.js',
+                'resources/css/user_styles/login_styles.css',
+                'resources/css/user_styles/register_styles.css',
                 'resources/css/user_styles/user-index_styles.css',
                 'resources/css/partials_styles/header_styles.css',
                 'resources/css/partials_styles/footer_styles.css',
