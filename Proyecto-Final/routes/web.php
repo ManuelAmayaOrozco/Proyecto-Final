f2d8d1604d2f9a73cfe8c81ce2c60fb51ed27da0
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-06-04 12:21:27 +0200

    Ruta de prueba para verificar las imagenes

--- Proyecto-Final/routes/web.php
+++ Proyecto-Final/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Mews\Captcha\Captcha;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', [PostController::class, 'showHome'])->name('home');
 
@@ -21,4 +22,8 @@ Route::get('captcha-reload', function () {
 
 Route::get('/check-manifest', function () {
     dd(file_exists(public_path('build/manifest.json')));
+});
+
+Route::get('/debug-images', function () {
+    return Storage::disk('public')->files('insects');
 });
