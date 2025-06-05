9c8bd0a05a4724054b9657f2c52580be2b75d9ce
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-06-05 14:08:44 +0200

    Otro intento

--- Proyecto-Final/resources/views/partials/profile-user.blade.php
+++ Proyecto-Final/resources/views/partials/profile-user.blade.php
@@ -72,4 +72,14 @@
 
     </div>
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
 </main>
