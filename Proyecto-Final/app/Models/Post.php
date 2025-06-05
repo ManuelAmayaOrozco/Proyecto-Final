6df8643d30434b31e9f9fc3c3e443b14d50b19a8
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-06-05 12:45:54 +0200

    Actualizacion deleteCompletely

--- Proyecto-Final/app/Models/Post.php
+++ Proyecto-Final/app/Models/Post.php
@@ -58,22 +58,6 @@ class Post extends Model
         // ELIMINAMOS LOS COMENTARIOS
         $this->comments()->delete();
 
-        // ELIMINAMOS FOTO DE IMGBB SI HAY DELETE_URL
-        if (!empty($this->photo) && filter_var($this->photo, FILTER_VALIDATE_URL)) {
-            if (!empty($this->photo_delete_url)) {
-                try {
-                    Http::get($this->photo_delete_url);
-                } catch (\Exception $e) {
-                    \Log::error('Error eliminando imagen de ImgBB: ' . $e->getMessage());
-                }
-            }
-        } else {
-            // Si es una foto local
-            if (Storage::disk('public')->exists($this->photo)) {
-                Storage::disk('public')->delete($this->photo);
-            }
-        }
-
         // OBTENEMOS Y ELIMINAMOS LAS ETIQUETAS ÚNICAS
         $uniqueTags = TagController::getAllUniqueTags($this->id);
         DB::table('post_tag')->where('post_id', $this->id)->delete();
